11ba1b165271a57398e943f86e8d6cb3b465bbf1
Rostyslav B. <bykov.rg@gmail.com>
2023-10-12 17:30:55 +0300

    feat: added mgu-2-course-oop-lection-8 apps

---
+++ 2023-2024/2-course/mgu-2-course-oop-lection-8-package-import-app/src/mypack/B.java
@@ -0,0 +1,12 @@
+package mypack;
+
+public class B {
+
+    public static void main(String[] args) {
+        pack.A obj = new pack.A();
+        obj.msg();
+
+        pack.C cObj = new pack.C();
+        cObj.msg();
+    }
+}
